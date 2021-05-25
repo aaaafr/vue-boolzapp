1031469520a300b34cf923b455423e4df81702c3
aaaafr <anastasia.frassini@icloud.com>
2021-05-25 15:18:02 +0200

    milestone 4 complete

--- milestone-4/main.js
+++ milestone-4/main.js
@@ -227,17 +227,3 @@ const app = new Vue({
 
 
 });
-
-
-// var listaOrdinata = cognomi.slice();
-// listaOrdinata.sort();
-// console.log(listaOrdinata);
-
-// // for(var i = 0; i < listaOrdinata.length; i++){
-// //     var cognome = listaOrdinata[i];
-// //     console.log(cognome);
-
-// // }
-
-// var posizione = listaOrdinata.indexOf(utenteCognome) + 1;
-// console.log(posizione);
